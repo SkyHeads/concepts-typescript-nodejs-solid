3dc0f7cea45f9ea5fcb60e3f4bc07cb70ae312aa
SkyHeads <skyheads@live.com>
2020-04-16 01:36:57 -0300

    Some solid concepts in a typescript API

---
+++ src/routes/appointmentsRoutes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentRepository';
+import CreateAppointmentService from '../services/createAppointmentService';
+
+const appointmentRouter = Router();
+
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentRouter.get('/', (request, response) => {
+  const appointment = appointmentsRepository.list();
+
+  return response.json(appointment);
+});
+
+appointmentRouter.post('/', (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentRouter;
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+const PORT = 3333;
+app.listen(3333, () => {
+  console.log(`Server Started on ${PORT}`);
+});
