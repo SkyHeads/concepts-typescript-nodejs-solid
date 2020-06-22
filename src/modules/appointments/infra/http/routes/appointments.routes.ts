3c111157e5209a1b405713b7703281948ac6cbd9
Bruno Fernandes <skyheads@live.com>
2020-06-22 15:07:56 -0300

    remove route list all appointments

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -8,14 +8,6 @@ const appointmentsController = new AppointmentsController();
 
 appointmentRouter.use(ensureAuthenticated);
 
-// appointmentRouter.get('/', async (request, response) => {
-//   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-
-//   const appointment = await appointmentsRepository.find();
-
-//   return response.json(appointment);
-// });
-
 appointmentRouter.post('/', appointmentsController.create);
 
 export default appointmentRouter;
